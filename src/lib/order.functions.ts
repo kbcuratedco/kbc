314fddcb6f8dda3e3c9fee1875ca5bdbc3df6d1c
Curtis Billnoske <curtisbillnoske@Curtiss-MacBook-Pro.local>
2026-07-30 19:13:30 -0500

    Fix Resend import

--- src/lib/order.functions.ts
+++ src/lib/order.functions.ts
@@ -1,6 +1,6 @@
 import { createServerFn } from "@tanstack/react-start";
 import { z } from "zod";
-import Resend from "resend";
+import { Resend } from "resend";
 
 const personalizationSchema = z
   .object({
